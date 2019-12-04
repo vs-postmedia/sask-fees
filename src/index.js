4cfce17bfdde66608efc574750ee958f9a3ca5d3
Nathan Griffiths <49567130+vs-postmedia@users.noreply.github.com>
2019-12-04 12:30:16 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App/App';
+import * as serviceWorker from './serviceWorker';
+
+// CSS
+import './css/index.css';
+
+// FONTS
+import './fonts/Shift-Bold.otf';
+import './fonts/Shift-BoldItalic.otf';
+import './fonts/Shift-BoldItalic.otf';
+import './fonts/BentonSansCond-Regular.otf';
+import './fonts/BentonSansCond-RegItalic.otf';
+import './fonts/BentonSansCond-Bold.otf';
+import './fonts/BentonSansCond-BoldItalic.otf';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
