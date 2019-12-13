1b1199b91e0233c020ed19b2af58dd27306f4229
Nathan Griffiths <ngriffiths@postmedia.com>
2019-12-13 14:25:35 -0800

    refactored for more adaptable layout

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,19 +3,22 @@ import './Card.css';
 
 const Card = (props) => {
 	const d = props.data;
-	let pageCost = '';
+	let pageCost, otherFees;
 
 	// total cost
-	const totalCost = d.total_cost !== '' ? d.total_cost : 'No reponse';
+	let totalCost = d.total_cost;
+	if (d.total_cost === '') {
+		totalCost = 'N/A';
+	} else if (d.total_cost !== 'Not provided') {
+		totalCost = `$${d.total_cost}`;
+	}
 
 	// conditionally show page costs & other fees
-	if (parseFloat(d.total_cost) > 0) {
-		pageCost = (
-			<div className="cost-details">
-				<p>{`Cost/page: ${d.page_cost}`}</p>
-				<p>{`Other fees: ${d.other_fees}`}</p>
-			</div>
-		)
+	if (d.other_fees !== '') {
+		otherFees = <p className="fee-details">{`Other fees: ${d.other_fees}`}</p>
+	}
+	if (parseFloat(d.page_cost) > 0) {
+		pageCost = <p className="fee-details">{`Cost/page: $${d.page_cost}`}</p>;
 	}
 
 	// same for document notes 
@@ -36,16 +39,15 @@ const Card = (props) => {
 			<div className="data-section">
 				<div className="cost">
 					<h3 className="day">Total cost</h3>
-					<p className="time">{`$${totalCost}`}</p>
-					
+					<p className="time">{`${totalCost}`}</p>
+					{pageCost}
 				</div>
 				<div className="response">
 					<h3>Response time</h3>
 					<p>{d.response_time}</p>
+					{otherFees}
 				</div>
 			</div>
-
-			{pageCost}
 
 			{note}
 		</div>
