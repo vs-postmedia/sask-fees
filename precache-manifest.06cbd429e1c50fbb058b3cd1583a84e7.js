6c22751d90a1cd93a6997e02ae96391116ecccf4
Nathan Griffiths <ngriffiths@postmedia.com>
2019-12-18 10:45:26 -0800

    Updates

---
+++ precache-manifest.06cbd429e1c50fbb058b3cd1583a84e7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613bd1fb8d605ad27685c04ae2450fb8",
+    "url": "/sask-fees/index.html"
+  },
+  {
+    "revision": "f76bbf8e1485ba7d64f7",
+    "url": "/sask-fees/static/css/main.c098f31e.chunk.css"
+  },
+  {
+    "revision": "a47213b21f3a1a5035b2",
+    "url": "/sask-fees/static/js/2.9b573f2d.chunk.js"
+  },
+  {
+    "revision": "9d4358a61b795e41849614fa47b5d1f8",
+    "url": "/sask-fees/static/js/2.9b573f2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f76bbf8e1485ba7d64f7",
+    "url": "/sask-fees/static/js/main.2ccc4db1.chunk.js"
+  },
+  {
+    "revision": "4265176ffced7dfb7a5a",
+    "url": "/sask-fees/static/js/runtime-main.9106a219.js"
+  },
+  {
+    "revision": "6f6753fd733a216b24786a168657540a",
+    "url": "/sask-fees/static/media/BentonSansCond-Bold.6f6753fd.otf"
+  },
+  {
+    "revision": "a6640d8951e3d6c523b2b58b0db9e351",
+    "url": "/sask-fees/static/media/BentonSansCond-BoldItalic.a6640d89.otf"
+  },
+  {
+    "revision": "69a4217c7da7462a2511b1a8ecaefecf",
+    "url": "/sask-fees/static/media/BentonSansCond-RegItalic.69a4217c.otf"
+  },
+  {
+    "revision": "43cd4ac75abf53e257d21366d3322b0e",
+    "url": "/sask-fees/static/media/BentonSansCond-Regular.43cd4ac7.otf"
+  },
+  {
+    "revision": "cdd38a860699d029f95e76f21e51e216",
+    "url": "/sask-fees/static/media/Shift-Bold.cdd38a86.otf"
+  },
+  {
+    "revision": "5c3edbfdeedc59bb35fac3bf1a461926",
+    "url": "/sask-fees/static/media/Shift-BoldItalic.5c3edbfd.otf"
+  }
+]);
